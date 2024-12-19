26dbe30e838548e458cf87ff5f8af65e94335ea7
StephenAraka <stevenaraka@gmail.com>
2024-12-20 00:26:57 +0300

    Setup tailwind

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    plugins: [
+      ['module:react-native-dotenv']
+    ],
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+  };
+};
