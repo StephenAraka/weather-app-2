05dcdf6d4edad0c2aa9f14723c76e9df1a8bd04c
StephenAraka <stevenaraka@gmail.com>
2024-12-20 03:48:08 +0300

    Add icons

---
+++ constants/index.ts
@@ -0,0 +1,15 @@
+export const weatherBackground = {
+  sunny: require("../assets/images/weather/sunny.png"),
+  cloudy: require("../assets/images/weather/cloudy.png"),
+  rainy: require("../assets/images/weather/rainy.png"),
+};
+
+export const icons = {
+  clear: require("../assets/icons/clear.png"),
+  partlysunny: require("../assets/icons/partlysunny.png"),
+  rain: require("../assets/icons/rain.png"),
+  menudots: require("../assets/icons/menu-dots.png"),
+  favorite: require("../assets/icons/favorite.png"),
+  info: require("../assets/icons/info.png"),
+  plus: require("../assets/icons/plus.png"),
+};
