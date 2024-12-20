347d0b4708a1b094f372a28a3bf367d5e96ab9ad
StephenAraka <stevenaraka@gmail.com>
2024-12-20 03:48:54 +0300

    Create weather component

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import CurrentWeather from '@/components/CurrentWeather';
+import Forecast from '@/components/Forecast';
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView className=" flex flex-1" edges={['left', 'right']}>
+      <CurrentWeather />
+      <Forecast />
+    </SafeAreaView>
   )
 }
 
