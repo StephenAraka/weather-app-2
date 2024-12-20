09e05b7102410757d997780ea4ecaa4360e0da91
StephenAraka <stevenaraka@gmail.com>
2024-12-20 13:30:36 +0300

    Add api calls

---
+++ lib/api.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+export const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+
+// Axios instance
+export const axiosInstance = axios.create({
+  baseURL: WEATHER_API_URL,
+  params: {
+    units: 'metric',
+    appid: WEATHER_API_KEY,
+  },
+});
+
+
+export const fetchWeatherData = async (latitude: number, longitude: number) => {
+  const [currentWeatherResponse, forecastResponse] = await Promise.all([
+    axiosInstance.get('/weather', {
+      params: { lat: latitude, lon: longitude },
+    }),
+    axiosInstance.get('/forecast', {
+      params: { lat: latitude, lon: longitude },
+    }),
+  ]);
+
+  return {
+    currentWeather: currentWeatherResponse.data,
+    forecast: forecastResponse.data,
+  };
+};
