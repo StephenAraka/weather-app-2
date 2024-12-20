00a8db9164fb4d1f2e5a3af9264fb795ee106d1b
StephenAraka <stevenaraka@gmail.com>
2024-12-20 05:07:19 +0300

    Get users device location

--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,17 +1,48 @@
 import CurrentWeather from '@/components/CurrentWeather';
 import Forecast from '@/components/Forecast';
-import React from 'react';
+import * as Location from 'expo-location';
+import React, { useEffect, useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const weather = 'sunny';
 
 const Home = () => {
+  const [location, setLocation] = useState<Location.LocationObject | null>(
+    null
+  );
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function getCurrentLocation() {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      console.log('====================================');
+      console.log(location);
+      console.log('====================================');
+      setLocation(location);
+    }
+
+    getCurrentLocation();
+  }, []);
+
+  let text = 'Waiting...';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
   return (
     <SafeAreaView className=" flex flex-1" edges={['left', 'right']}>
       <CurrentWeather weather={weather} />
       <Forecast weather={weather} />
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
