8f4ad3e09642739bc2f64d183e60a80f3d439c6e
StephenAraka <stevenaraka@gmail.com>
2025-01-04 14:22:26 +0300

    Prompt AI

--- components/WeatherBot.tsx
+++ components/WeatherBot.tsx
@@ -14,34 +14,43 @@ type WeatherBotProps = {
 
 const WeatherBot = ({ feelsLike, location, type }: WeatherBotProps) => {
   const [showTooltip, setShowTooltip] = useState(true);
+  const [aiResponse, setAiResponse] = useState('');
+  const [promptingAI, setPromptingAI] = useState(false);
+  const tooltipTimeout = aiResponse ? 60000 : 11000;
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowTooltip(false);
-    }, 11000);
+    }, tooltipTimeout);
 
     return () => clearTimeout(timer);
   }, []);
 
   const handleLinkPress = async () => {
     try {
-      const genAI = new GoogleGenerativeAI.GoogleGenerativeAI(GOOGLE_AI_API_KEY);
+      const genAI = new GoogleGenerativeAI.GoogleGenerativeAI(
+        GOOGLE_AI_API_KEY
+      );
       const model = genAI.getGenerativeModel({
-        model: 'gemini-1.5-flash'
+        model: 'gemini-1.5-flash',
       });
 
-      const prompt = `The weather in ${location} has ${type} and currently feels like ${feelsLike}°C. What would you recommend for such weather in case I am moving out maybe? What do you think I should wear or carry or consider?`;
+      const prompt = `The weather in ${location} has ${type} and currently feels like ${feelsLike}°C. What would you recommend for such weather in case I am moving out maybe? What do you think I should wear or carry or consider? Make the response a little short, though. About 50 words max. Add a little sense of humor`;
 
-      const result = await model.generateContent(
-        prompt);
+      setPromptingAI(true);
+
+      const result = await model.generateContent(prompt);
 
       // Assuming `result.content` holds the generated response.
       if (result && result.response) {
-        console.log('Generated Response:', result.response.text());
+        setPromptingAI(false);
+        setAiResponse(result.response.text());
       } else {
+        setPromptingAI(false);
         console.error('No content received from the model.');
       }
     } catch (error) {
+      setPromptingAI(false);
       console.error('Error generating content:', error);
     }
   };
@@ -50,14 +59,27 @@ const WeatherBot = ({ feelsLike, location, type }: WeatherBotProps) => {
     <View className="items-end absolute bottom-4 right-4">
       {showTooltip && (
         <View className="bg-white p-4 rounded-lg shadow-md mb-[-24] w-64 relative">
-          <Text className="text-gray-800 text-sm">
-            Hi 👋🏻, my name is Luna, and I can tell you what I think about your
-            weather today!! Want to know what I think? 🤔💭{' '}
-            <Text className="text-blue-500 underline" onPress={handleLinkPress}>
-              click here
+          {aiResponse && !promptingAI && (
+            <Text className="text-gray-800 text-sm">{aiResponse}</Text>
+          )}
+          
+          {!aiResponse && !promptingAI && (
+            <Text className="text-gray-800 text-sm">
+              Hi 👋🏻, my name is Luna, and I can tell you what I think about your
+              weather today!! Want to know what I think? 🤔💭{' '}
+              <Text
+                className="text-blue-500 underline"
+                onPress={handleLinkPress}
+              >
+                click here
+              </Text>
+              .
             </Text>
-            .
-          </Text>
+          )}
+
+          {promptingAI && (
+            <Text className="text-gray-800 text-sm">Hold on, let me think 🤔...</Text>
+          )}
           <View className="absolute bottom-[-6px] right-4 w-4 h-4 bg-white rotate-45" />
         </View>
       )}
